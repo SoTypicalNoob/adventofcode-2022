d5e2e7da1a0725de70110dc6add6be7f6cacdb7a
SoTypicalNoob <so.typical.noob@gmail.com>
2023-04-30 09:34:55 +0200

    Finishing 2015-day-1

---
+++ 2015/day-01.lisp
@@ -0,0 +1,37 @@
+(ql:quickload "str")
+
+(defun input (input-file)
+  (uiop:read-file-lines input-file))
+
+(defun day-01-1 (input-file)
+  (let* ((input (input input-file))
+         (flr 0)
+         (cnt 0))
+    (loop :for line :in input
+          :do (loop :for item :across line
+                    :do (if (string= item "(")
+                            (incf flr)
+                            (decf flr))
+                        (incf cnt)
+                        (if (= flr -1)
+                            (format t "~A" cnt)
+                            )))
+    flr))
+
+(day-01-1 "inputs/input-01")
+
+(defun day-01-2 (input-file)
+  (let* ((input (input input-file))
+         (flr 0)
+         (cnt 0))
+    (loop :for line :in input
+          :do (loop :for item :across line
+                    :do (if (string= item "(")
+                            (incf flr)
+                            (decf flr))
+                        (incf cnt)
+                    :when (= flr -1)
+                      :return cnt))
+    cnt))
+
+(day-01-2 "inputs/input-01")
